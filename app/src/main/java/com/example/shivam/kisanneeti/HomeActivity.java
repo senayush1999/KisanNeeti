0eecf925dface07a60a38631b260bf2fc8d46886
Shivam Mahajan <Shivam.mahajan37@gmail.com>
2018-11-04 01:47:27 +0530

    Initial Commit

--- app/src/main/java/com/example/shivam/kisanneeti/HomeActivity.java
+++ app/src/main/java/com/example/shivam/kisanneeti/HomeActivity.java
@@ -65,9 +65,7 @@ public class HomeActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
-
     }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
